df4255a3278f44b92d9729d7826bea6d02ef88fd
Vela Juarez <vovazms@gmail.com>
2025-06-23 12:31:17 +0300

    Added movement for amoeba

--- assets/objects/player/player.gd
+++ assets/objects/player/player.gd
@@ -2,12 +2,18 @@ extends CharacterBody2D
 
 const SPEED = 100.0
 
+@onready var anim_player = $AnimatedSprite2D
+
 func _process(delta):
 	move_player()
 	
 	
 func move_player():
-	var direction: Vector2 = Vector2(Input.get_axis("left", "right"), Input.get_axis("up", "down")).normalized()
-	velocity = direction * SPEED
-	move_and_slide()
+	velocity = Vector2(Input.get_axis("left", "right"), Input.get_axis("up", "down")).normalized() * SPEED
+	if velocity.length() != 0:
+		anim_player.play("moving")
+		move_and_slide()
+	else:
+		anim_player.play("idle")
+		
 	
