cdd6b8acfba98424588022d4c4294c13449ef788
Vela Juarez <vovazms@gmail.com>
2025-08-05 13:29:29 +0300

    Added destructible component

--- components/DestructibleComponent.cs
+++ components/DestructibleComponent.cs
@@ -1,8 +1,39 @@
 using Godot;
 using System;
 
-public partial class DestructibleComponent : Node
+public partial class DestructibleComponent : Node, IDestructible
 {
     [ExportGroup("Audio")]
-    [Export] public AudioStream DamageSound { get; set; }
+    [Export] public AudioStream DeathSound { get; set; }
+
+    [ExportGroup("Effects")]
+    [Export] public PackedScene DeathEffectScene { get; set; }
+
+    private bool _isDead = false;
+
+    private AudioStreamPlayer _audioPlayer;
+
+    public override void _Ready()
+    {
+        base._Ready();
+
+        _audioPlayer = new AudioStreamPlayer();
+        AddChild(_audioPlayer);
+    }
+
+    public void TakeDamage()
+    {
+        if (!_isDead) {
+            Die();
+        }
+    }
+
+    private void Die()
+    {
+        if (_isDead) return;
+        _isDead = true;
+
+        if (parent)
+    }
+
 }
