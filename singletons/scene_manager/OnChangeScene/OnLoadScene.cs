141f5f17dfed4baef77c5e91195ea0fc98f92c72
Vela Juarez <vovazms@gmail.com>
2025-08-25 12:09:11 +0300

    1

--- singletons/scene_manager/OnChangeScene/OnLoadScene.cs
+++ singletons/scene_manager/OnChangeScene/OnLoadScene.cs
@@ -4,17 +4,11 @@ using System.Threading.Tasks;
 
 public partial class OnLoadScene : CanvasLayer
 {
-    // Сигнал, который мы отправим SceneManager'у, когда ВСЕ будет готово.
-    // Он несет в себе массив уже загруженных сцен.
-    [Signal]
-    public delegate void AllScenesLoadedEventHandler(Array<PackedScene> loadedScenes);
+    [Signal] public delegate void AllScenesLoadedEventHandler(Array<PackedScene> loadedScenes);
 
-    [Signal]
-    private delegate void LoadingFinishedEventHandler();
+    [Signal] private delegate void LoadingFinishedEventHandler();
 
-    [Export]
     private AnimationPlayer _animationPlayer;
-    [Export]
     private TextureProgressBar _progressBar;
 
     private Array<string> _scenesToLoadPaths;
@@ -26,6 +20,10 @@ public partial class OnLoadScene : CanvasLayer
         SceneManager.Instance.LoadingStarted += StartLoadingScenes;
         AllScenesLoaded += OnAllScenesLoaded;
         ProcessMode = ProcessModeEnum.Disabled;
+
+        _animationPlayer = GetNode<AnimationPlayer>("AnimationPlayer");
+        _progressBar = GetNode<TextureProgressBar>("ColorRect/CenterContainer/VBoxContainer/TextureProgressBar");
+
     }
 
     public async void StartLoadingScenes(Array<string> scenePaths)
@@ -33,37 +31,28 @@ public partial class OnLoadScene : CanvasLayer
         _scenesToLoadPaths = scenePaths;
 
         ProcessMode = ProcessModeEnum.Always;
-
-        GD.Print(ProcessMode);
         
-        // 1. Показываем анимацию "затемнения"
         _animationPlayer.Play("Dissolve");
         await ToSignal(_animationPlayer, AnimationPlayer.SignalName.AnimationFinished);
 
-        // 2. Начинаем фоновую загрузку и включаем _Process для обновления ProgressBar
         foreach (var path in _scenesToLoadPaths)
         {
             ResourceLoader.LoadThreadedRequest(path);
         }
         
-        // 3. ЖДЕМ, пока _Process не сообщит нам, что загрузка завершена
         await ToSignal(this, SignalName.LoadingFinished);
         
-        // 4. Собираем результаты
         var loadedScenes = new Array<PackedScene>();
         foreach (var path in _scenesToLoadPaths)
         {
             loadedScenes.Add((PackedScene)ResourceLoader.LoadThreadedGet(path));
         }
 
-        // 5. Сообщаем SceneManager'у, что все готово и передаем ему сцены
         EmitSignal(SignalName.AllScenesLoaded, loadedScenes);
 
-        // 6. Запускаем анимацию "проявления"
         _animationPlayer.PlayBackwards("Dissolve");
         await ToSignal(_animationPlayer, AnimationPlayer.SignalName.AnimationFinished);
         
-        // 7. Самоуничтожаемся
         QueueFree();
     }
 
@@ -88,7 +77,6 @@ public partial class OnLoadScene : CanvasLayer
     
     public override void _Process(double delta)
     {
-        // Если флаг уже взведен, ничего не делаем
         if (_loadingFinished) return;
 
         float totalProgress = 0f;
@@ -110,13 +98,11 @@ public partial class OnLoadScene : CanvasLayer
             }
         }
 
-        // Правильный подсчет общего прогресса
         _progressBar.Value = (totalProgress / _scenesToLoadPaths.Count) * 100;
 
-        // Если все сцены загружены
         if (loadedCount == _scenesToLoadPaths.Count)
         {
-            _loadingFinished = true; // Взводим флаг, чтобы не испускать сигнал много раз
+            _loadingFinished = true;
             EmitSignal(SignalName.LoadingFinished);
         }
     }
